19de727290b7adc03a6914da5077b80ed654bf46
Khushi Shah <81346602+KShah2089@users.noreply.github.com>
2021-11-19 10:45:41 +0530

    Penultimate Version

---
+++ Snake.java
@@ -0,0 +1,122 @@
+package game;
+import java.util.*;
+
+
+public class Snake {
+	private LinkedList<Square> snake;
+	private int direction;
+	public static final int NORTH = 0, EAST = 1, SOUTH = 2, WEST = 3;
+	
+	Snake() {
+		snake = new LinkedList<Square>();
+		Square head = new Square(2,0);
+		Square t1 = new Square(1,0);
+		Square t2 = new Square(0,0);
+		head.makeSNAKE();
+		t1.makeSNAKE();
+		t2.makeSNAKE();
+		snake.add(head);
+		snake.add(t1);
+		snake.add(t2);
+		direction = EAST;
+	}
+
+	private boolean hitBoundary(int n) {
+		boolean hit = false;
+		Square head = getHead();
+		if (direction == EAST) {
+			hit = head.getX() == n-1;
+		}
+		else if (direction == SOUTH) {
+			hit = head.getY() == n-1;
+		}
+		else if (direction == NORTH)  {
+			hit = head.getY() == 0;
+		}
+		else {
+			hit = head.getX() == 0;
+		}
+		return hit;
+	}
+
+	private boolean hitItself() {
+		boolean hit = false;
+		Square head = getHead();
+		int x_death, y_death;
+		if (direction == EAST) {
+//			x_death = head.getX();
+//			y_death = head.getY()+1;
+			x_death = head.getX()+1;
+			y_death = head.getY();
+		}
+		else if (direction == SOUTH) {
+//			x_death = head.getX()+1;
+//			y_death = head.getY();
+			x_death = head.getX();
+			y_death = head.getY()+1;
+		}
+		else if (direction == NORTH)  {
+//			x_death = head.getX()-1;
+//			y_death = head.getY();
+			x_death = head.getX();
+			y_death = head.getY()-1;
+		}
+		else {
+//			x_death = head.getX();
+//			y_death = head.getY()-1;
+			x_death = head.getX()-1;
+			y_death = head.getY();
+		}
+		for (int i = 1; i < snake.size(); i++) {
+			if (snake.get(i).getX() == x_death && snake.get(i).getY() == y_death) {
+				hit = true;
+				break;
+			}
+		}
+		return hit;
+	}
+		
+	public boolean gameOver(int n) {
+		return hitBoundary(n) || hitItself();
+	}
+	
+	public void moveOne() {
+		snake.removeLast();
+		Square head = this.getHead();
+		if (direction == NORTH) {
+			snake.addFirst(new Square(head.getX(), head.getY()-1));
+		}
+		else if (direction == EAST) {
+			snake.addFirst(new Square(head.getX()+1, head.getY()));
+		}
+		else if (direction == SOUTH) {
+			snake.addFirst(new Square(head.getX(), head.getY()+1));
+		}
+		else {
+			snake.addFirst(new Square(head.getX()-1, head.getY()));
+		}		
+	}
+	
+	public void growOne() {
+		Square head = this.getHead();
+		if (direction == NORTH) {
+			snake.addFirst(new Square(head.getX(), head.getY()-1));
+		}
+		else if (direction == EAST) {
+			snake.addFirst(new Square(head.getX()+1, head.getY()));
+		}
+		else if (direction == SOUTH) {
+			snake.addFirst(new Square(head.getX(), head.getY()+1));
+		}
+		else {
+			snake.addFirst(new Square(head.getX()-1, head.getY()));
+		}
+	}
+	
+	public int getDirection() { return direction; }
+	public void setDirection(int direction) { this.direction = direction; }
+	public LinkedList<Square> getSnake() { return snake; }
+	public Square getHead() { return snake.getFirst(); }
+	public int getLength() { return snake.size(); }
+	
+}
---
+++ SuccessfullLogin.java
@@ -0,0 +1,124 @@
+package game;
+
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SuccessfullLogin extends JFrame implements ActionListener{
+	
+	private int size = 30, speed = 140;
+	
+	public SuccessfullLogin() {
+		JFrame frame = new JFrame();
+		//frame.setLocationRelativeTo(null);
+
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(300,300,300,300));
+		panel.setLayout(new GridLayout(0,1));
+
+		JButton button1 = new JButton("PLAY");
+		JButton button2 = new JButton("CONTROLS");
+		
+		button1.addActionListener(this);
+		
+		panel.add(button1);
+
+		JButton b30 = new JButton("30x30");
+		JButton b40 = new JButton("40x40");
+		JButton b50 = new JButton("50x50");
+		JButton b60 = new JButton("60x60");
+		JButton b70 = new JButton("70x70");
+		
+		b30.addActionListener(new b30());
+		b40.addActionListener(new b40());
+		b50.addActionListener(new b50());
+		b60.addActionListener(new b60());
+		b70.addActionListener(new b70());
+		
+		panel.add(b30);
+		panel.add(b40);
+		panel.add(b50);
+		panel.add(b60);
+		panel.add(b70);
+		
+		JButton moderate = new JButton("moderate");
+		JButton fast = new JButton("fast");
+		JButton slow = new JButton("slow");
+		
+		panel.add(moderate);
+		panel.add(fast);
+		panel.add(slow);
+		
+		moderate.addActionListener(new moderate());
+		fast.addActionListener(new fast());
+		slow.addActionListener(new slow());
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("HOMEPAGE");
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	private class b30 implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			size = 30;
+		}
+	}
+	private class b40 implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			size = 40;
+		}
+	}
+	private class b50 implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			size = 50;
+		}
+	}
+	private class b60 implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			size = 60;
+		}
+	}
+	private class b70 implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			size = 70;
+		}
+	}
+	
+	private class moderate implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			speed = 140;
+		}
+	}
+	private class fast implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			speed = 100;
+		}
+	}
+	private class slow implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			speed = 180;
+		}
+	}
+	
+	
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO add option for controls
+		add(new GameBoard(size, speed));
+		setResizable(false);
+		pack();
+		setTitle("Snake");
+		setLocationRelativeTo(null);
+		// TODO add option to play again
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
